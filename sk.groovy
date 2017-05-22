4f5d044a53360aa321b4279582e26cfebf27e87e
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2017-05-21 21:52:43 -0700

    Adding automatic update to the sk.groovy script

--- sk.groovy
+++ sk.groovy
@@ -1,4 +1,6 @@
 #!/usr/bin/env groovy
 
+"${System.getProperty('user.home')}/.sk/tools/check-for-update.sh".execute().waitForProcessOutput(System.out, System.err)
+
 println 'skeleton-cli'
 args.each { println it }
